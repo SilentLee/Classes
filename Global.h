627460c923e1365e352a16dd145e7ce5c8bd009f
SilentLee <1378180704@qq.com>
2017-12-11 20:32:21 +0800

    20171211
    同步代码

--- Global.h
+++ Global.h
@@ -21,12 +21,7 @@
 #define DEFAULT_POWER_BUFFER_LENGTH     8 // FOR INT
 
 // 服务器参数
-//#define IP_ADDRESS "169.254.43.180"       //  李熙良的 Mac
-//#define IP_ADDRESS "172.20.10.5"       //  李熙良的 iPhone
-//#define IP_ADDRESS "192.168.0.103"        // 华业
-//#define IP_ADDRESS "192.168.1.107"        // 东一时区
-//#define IP_ADDRESS "192.168.0.122"
-#define IP_ADDRESS "192.168.1.165"
+#define IP_ADDRESS "192.168.0.107"
 #define IP_PORT     6000
 
 // 游戏文件路径
