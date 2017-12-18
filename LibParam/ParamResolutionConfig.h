441388a06522926eb15f3d70f38a21013feddb7a
SilentLee <1378180704@qq.com>
2017-12-18 09:00:54 +0800

    20171218
    错误恢复

--- LibParam/ParamResolutionConfig.h
+++ LibParam/ParamResolutionConfig.h
@@ -6,3 +6,5 @@
 static cocos2d::Size debugResolutionSize = cocos2d::Size(540, 960);
 // 游戏设计分辨率
 static cocos2d::Size designResolutionSize = cocos2d::Size(1080, 1920);
+
+static float frameSizeFactor = 1.0f;
